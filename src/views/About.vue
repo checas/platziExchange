779829813bc96365640a51bc4cba29e5b6472780
Sergio <Checcarelli>
2020-11-22 14:07:28 -0300

    ...

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex-col items-center">
     <h1 class="text-gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
-    <p class="text-gray-600 text-xl text-center">
+    <p class="text-gray-600 text-xl text-center text-red-50">
       Proyecto para obtener las cotizaciones de las cryptomonedas mas
       importantes a traves de la API REST de Coincap.
       <br />Este proyecto es utilizado en el curo de Vue.js Basico de Platzi
